9ebd7b04c448184b3c1f92a81888955b7374b0eb
Vladimir174 <magvaivladimir@gmail.com>
2024-09-26 14:59:49 +0300

    Update game.js

--- game.js
+++ game.js
@@ -3,56 +3,98 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
+
+//Флаг для проверки началась игра или нет, если началась. Для вызова функции nextSequence() только при
+//первом нажатии клавиши.
 var started = false;
+
+//Уровень
 var level = 0;
+
+//Ожидание нажатие кнопка А.
+$(document).keydown(function () {
+  if (!started) {
+    //проверяем запущена ли игра, смотрим на флаг.
+
+    //Меняем надпись когда была начата игра и нажата клавиша.
+    $("#level-title").text("level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+//Играем звук при клике
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id"); //получаем значение цвета из выбраной нажатой кнопки
+  userClickedPattern.push(userChosenColour); //записываем в массив.
+
+  playSound(userChosenColour); //играем соответствующий файл
+  animatePress(userChosenColour); //анимация кнопки при нажатии
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length) {
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game-Over, нажмите любую клавишу для рестарта.");
+    startOver();
+  }
+}
 
 //функция генерации случайного числа
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  playSound(randomChosenColour);
+  userClickedPattern = [];
+  //переключаем уровень
+  level++;
 
-  //Записываем в массим полученный случайный цвет
+  //меняем заголовок
+  $("#level-title").text("level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4); //генерим случайное число
+  var randomChosenColour = buttonColours[randomNumber]; //получаем из случайного числа, случайный цвет.
+  gamePattern.push(randomChosenColour); //записываем его в массив
+
+  //Воспроизводим анимацию для кнопки
   $("#" + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  level++;
-  $("h1").text("level " + level);
+  //играем соответствующий звук
+  playSound(randomChosenColour);
 }
-
-//Играем звук при клике
-$(".btn").on("click", function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-});
-
 //Функция воспроизведения нужного звука
 function playSound(name) {
   var audio = new Audio("./sounds/" + name + ".mp3");
-  audio.play().catch(function (err) {
-    console.error("Ошибка проигрывания звука:", err);
-  });
+  audio.play();
 }
 
 //Анимация кнопок
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
-//Ожидание нажатие кнопка А.
-$(document).on("keydown", function () {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
